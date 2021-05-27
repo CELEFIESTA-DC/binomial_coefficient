3f17a7244199419cc9081c4cd37f05597c39a28b
Muhammed Nazeem <39720973+NazeemNato@users.noreply.github.com>
2021-05-27 21:15:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-
+"""
+A binomial coefficient C(n, k) also gives the number of ways, disregarding order, 
+that k objects can be chosen from among n objects more formally, the number of k-element subsets (or k-combinations) of a n-element set
+eg:
+    >>> binomial_coefficient(n=10, r=5)
+    252
+"""
 def binomial_coefficient(n, r):
     coefficient = [0 for i in range(r - 1)]
     coefficient[0] = 1
